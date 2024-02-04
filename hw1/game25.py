abcc10f55de5c968fca315a1a7f47e7fd2d9ab3b
Josh Zhang <joshzhang@berkeley.edu>
2024-02-04 14:14:04 -0800

    working on hw3

--- hw1/game25.py
+++ hw1/game25.py
@@ -16,13 +16,3 @@ def generate_moves(post):
 
 def primitive_value(post):
     return "lose" if post == POSTS[0] else None
-
-def test_cases():
-    print("Testing do_move(position, 1):")
-    [print(f"{p}: {do_move(p, 1)}") for p in POSTS[1:]]
-
-    print("Testing generate_moves():")
-    [print(f"{p}: {generate_moves(p)}")  for p in POSTS[1:]]
-    
-    print("Testing primitive_value():")
-    [print(f"{p}: {primitive_value(p)}") for p in POSTS]
